ee164b8f2ce3255df9540520a082c636aa89a282
Dimitris <vervas.g@gmail.com>
2012-07-30 18:17:23 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmanager::Application.config.secret_token = 'fcae72abbdb01ff4128e740c4aded04027dd01627a94b0a1532e2291f1e173471ba2f73fc6cb0eebb1f96ba3c09f3aad37d62c19c56c90e28773ee10111c201c'
