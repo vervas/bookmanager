f5cd40e23162397a142695f3e51f893df11f6af5
Dimitris <vervas.g@gmail.com>
2012-10-02 17:34:32 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Bookmanager::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'books'
+   root :to => 'books#index.html.rb'
 
   # See how all your routes lay out with "rake routes"
 
